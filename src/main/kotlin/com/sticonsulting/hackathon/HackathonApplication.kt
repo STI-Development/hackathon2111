ba90a128fb8578b11430f0775608888e7ddd32d3
ugur <bayraktar@live.de>
2022-11-21 15:03:10 +0100

    damit man die app starten kann

--- src/main/kotlin/com/sticonsulting/hackathon/HackathonApplication.kt
+++ src/main/kotlin/com/sticonsulting/hackathon/HackathonApplication.kt
@@ -1,9 +1,10 @@
 package com.sticonsulting.hackathon
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class HackathonApplication
 
 fun main(args: Array<String>) {
