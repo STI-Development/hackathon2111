fdc0cd2bb17a1fa23825a28aca0d559d1c760d3f
Mohamad Schurbaji <mohamad.schurbaji@sti-consulting.com>
2022-11-22 10:26:04 +0100

    add field validation

---
+++ src/main/kotlin/com/sticonsulting/hackathon/config/RestExceptionHandler.kt
@@ -0,0 +1,22 @@
+package com.sticonsulting.hackathon.config
+
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class RestExceptionHandler : ResponseEntityExceptionHandler() {
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        return ResponseEntity.badRequest().body(ex.bindingResult.toString())
+    }
+}
--- src/main/kotlin/com/sticonsulting/hackathon/dto/JobApplicationDto.kt
+++ src/main/kotlin/com/sticonsulting/hackathon/dto/JobApplicationDto.kt
@@ -5,8 +5,8 @@ import javax.validation.constraints.Size
 
 data class JobApplicationDto(
     var id: Long?,
-    @NotBlank(message = "name is not allowed to be empty")
-    @Size(min = 3, max = 20, message = "Name should be between 3 - 20")
+    @field:NotBlank(message = "name is not allowed to be empty")
+    @field:Size(min = 3, max = 20, message = "Name should be between 3 - 20")
     var name: String,
     var active: Boolean
 )
